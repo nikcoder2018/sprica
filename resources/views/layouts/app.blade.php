f4e92e3a13896b64bd307afc6cb4493bab318260
Nick Jay <b.nickjay05@gmail.com>
2020-09-12 17:08:56 +0800

    add user scalable = no

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,9 +4,9 @@
     <title>{{App\Helpers\Language::settings('Admin_Title')}}</title>
 
     <!-- META SECTION -->
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8, user-scalable=no">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no">
 
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
