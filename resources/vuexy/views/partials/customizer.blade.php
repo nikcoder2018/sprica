b88a29405599a7c06657652b9e67810c593eae80
John Michael Manlupig <manlupigjohnmichael@gmail.com>
2021-01-03 12:43:22 +0800

    Fixed theme button sync at navbar

--- resources/vuexy/views/partials/customizer.blade.php
+++ resources/vuexy/views/partials/customizer.blade.php
@@ -240,6 +240,11 @@
         }
 
         const registerThemeModes = ($) => {
+            $('#theme-mode-button').click(() => {
+                const mode = $('#theme-mode-button').attr('data-status') === 'dark' ? 'light' : 'dark';
+                $(`#${mode}-theme-button`).click();
+            });
+
             (() => {
                 let running = false;
                 $('#light-theme-button').click((e) => {
@@ -248,6 +253,10 @@
                     } else {
                         return;
                     }
+                    $('#theme-mode-button').attr('data-status', 'light');
+                    $('#theme-mode-button').html(
+                        `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon ficon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>`
+                    );
                     $.post("{{ route('user.settings.store') }}", {
                         key: 'theme-mode',
                         value: 'light',
@@ -264,6 +273,10 @@
                     } else {
                         return;
                     }
+                    $('#theme-mode-button').attr('data-status', 'dark');
+                    $('#theme-mode-button').html(
+                        `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-sun ficon"><circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line></svg>`
+                    );
                     $.post("{{ route('user.settings.store') }}", {
                         key: 'theme-mode',
                         value: 'dark',
@@ -280,6 +293,10 @@
                     } else {
                         return;
                     }
+                    $('#theme-mode-button').attr('data-status', 'light');
+                    $('#theme-mode-button').html(
+                        `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon ficon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>`
+                    );
                     $.post("{{ route('user.settings.store') }}", {
                         key: 'theme-mode',
                         value: 'bordered',
@@ -431,11 +448,17 @@
                     }
                     if (key === 'theme-mode') {
                         $(`#skin${value}`).click();
+                        if (value !== 'dark') {
+                            $('#theme-mode-button').html(
+                                `<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-moon ficon"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path></svg>`
+                            );
+                            $('#theme-mode-button').attr('data-status', 'light');
+                        }
                     }
                     if (key === 'collapsible-mode') {
                         if (value === 'on') {
                             setTimeout(() => {
-
+                                //
                             }, 15000);
                         }
                     }
